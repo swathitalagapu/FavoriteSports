a90ff79f6e3712c9b9fd2577f0bf2902a01930eb
DESKTOP-JGUHHCD\Swathi <swathi.t@imaginnovate.com>
2023-06-28 10:55:39 +0530

    Initial commit

---
+++ FavoriteSports/src/main/java/com/example/FavoriteSports/FavoriteSportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FavoriteSports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteSportsApplication.class, args);
+	}
+
+}
